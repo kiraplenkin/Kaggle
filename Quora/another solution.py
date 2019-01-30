8ed3fdd9b07a1a502030d5bdfa54118fcb825df0
SK <myp2zz@yandex.ru>
2019-01-30 17:55:53 +0300

    Com

--- Quora/another solution.py
+++ Quora/another solution.py
@@ -4,9 +4,8 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 import math
-from sklearn.model_selection import train_test_split
-from sklearn import metrics
-from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split, StratifiedKFold
+from sklearn.metrics import f1_score
 
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
@@ -205,13 +204,10 @@ def load_data():
     # Shuffling the data
     np.random.seed(42)
     trn_idx = np.random.permutation(len(train_X))
-    val_idx = np.random.permutation(len(val_X))
     train_X = train_X[trn_idx]
-    val_X = val_X[val_idx]
     train_y = train_y[trn_idx]
-    val_y = val_y[val_idx]
 
-    return train_X, val_X, test_X, train_y, val_y, tokenizer.word_index
+    return train_X, test_X, train_y, tokenizer.word_index
 
 
 def load_glove(word_index):
@@ -311,7 +307,45 @@ def f1(y_true, y_pred):
     return 2 * ((precision * recall) / (precision + recall + K.epsilon()))
 
 
-def train_pred(model, epochs=2):
+def model_cnn(embedding_matrix):
+    filter_sizes = [1, 2, 3, 5]
+    num_filters = 72
+
+    inp = Input(shape=(MAXLEN,))
+    x = Embedding(MAX_FEATURES, EMBED_SIZE, weights=[embedding_matrix])(inp)
+    x = Reshape((MAXLEN, EMBED_SIZE, 1))(x)
+
+    maxpool_pool = []
+    for i in range(len(filter_sizes)):
+        conv = Conv2D(num_filters, kernel_size=(filter_sizes[i], EMBED_SIZE),
+                      kernel_initializer='he_normal', activation='elu')(x)
+        maxpool_pool.append(MaxPool2D(pool_size=(MAXLEN - filter_sizes[i] + 1, 1))(conv))
+
+    z = Concatenate(axis=1)(maxpool_pool)
+    z = Flatten()(z)
+    z = Dropout(0.1)(z)
+
+    outp = Dense(1, activation="sigmoid")(z)
+
+    model = Model(inputs=inp, outputs=outp)
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    return model
+
+
+def threshold_search(y_true, y_proba):
+    best_threshold = 0
+    best_score = 0
+    for threshold in [i * 0.01 for i in range(100)]:
+        score = f1_score(y_true=y_true, y_pred=y_proba > threshold)
+        if score > best_score:
+            best_threshold = threshold
+            best_score = score
+    search_result = {'threshold': best_threshold, 'f1': best_score}
+    return search_result
+
+
+def train_pred(model, train_X, train_y, val_X, val_y, epochs=2):
     for e in range(epochs):
         model.fit(train_X, train_y, batch_size=512, epochs=2, validation_data=(val_X, val_y))
         pred_val_y = model.predict([val_X], batch_size=1024, verbose=0)
@@ -320,7 +354,7 @@ def train_pred(model, epochs=2):
         best_score = 0.0
         for thresh in np.arange(0.1, 0.501, 0.01):
             thresh = np.round(thresh, 2)
-            score = metrics.f1_score(val_y, (pred_val_y > thresh).astype(int))
+            score = f1_score(val_y, (pred_val_y > thresh).astype(int))
             if score > best_score:
                 best_thresh = thresh
                 best_score = score
@@ -332,13 +366,35 @@ def train_pred(model, epochs=2):
     return pred_val_y, pred_test_y, best_score
 
 
-train_X, val_X, test_X, train_y, val_y, word_index = load_data()
+train_X, test_X, train_y, word_index = load_data()
 
 embedding_matrix_1 = load_glove(word_index)
-emdedding_matrix_2 = load_fasttext(word_index)
+#emdedding_matrix_2 = load_fasttext(word_index)
 embedding_matrix_3 = load_para(word_index)
 
 embedding_matrix = np.mean([embedding_matrix_1, embedding_matrix_3], axis=0)
 
+
+train_meta = np.zeros(train_y.shape)
+test_meta = np.zeros(test_X.shape[0])
+
+splits = list(StratifiedKFold(n_splits=3, shuffle=True, random_state=42).split(train_X, train_y))
+
+for idx, (train_idx, valid_idx) in enumerate(splits):
+        X_train = train_X[train_idx]
+        y_train = train_y[train_idx]
+        X_val = train_X[valid_idx]
+        y_val = train_y[valid_idx]
+        model = model_cnn(embedding_matrix)
+        pred_val_y, pred_test_y, best_score = train_pred(model, X_train, y_train, X_val, y_val, epochs=6)
+        train_meta[valid_idx] = pred_val_y.reshape(-1)
+        test_meta += pred_test_y.reshape(-1) / len(splits)
+
+search_result = threshold_search(train_y, train_meta)
+pred_model_cnn = test_meta > search_result['threshold']
+
+sub = pd.read_csv('../input/sample_submission.csv')
+sub.prediction = pred_model_cnn.astype(int)
+sub.to_csv("submission.csv", index=False)
 
 
